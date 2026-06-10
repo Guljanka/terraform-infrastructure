e70f4404845830734a3eaf2d5ce0d9161b5072e2
Guljanka <amir19ahmad21@gmail.com>
2026-06-09 20:06:18 -0500

    Remote leftover security group blocks

--- terraform-infrastructure/main.tf
+++ terraform-infrastructure/main.tf
@@ -82,21 +82,3 @@ resource "aws_ecs_cluster" "main" {
  name = "student-cluster"
 }
 
-resource "aws_security_group" "web" {
-  name   = "web-sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
