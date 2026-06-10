59b55d4aa17b3e1ac697e54595816084c10b7b48
Guljanka <amir19ahmad21@gmail.com>
2026-06-09 19:51:14 -0500

    Update main.tf

--- terraform-infrastructure/main.tf
+++ terraform-infrastructure/main.tf
@@ -80,18 +80,4 @@ resource "aws_lb" "website" {
 resource "aws_ecs_cluster" "main" {
 
  name = "student-cluster"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
